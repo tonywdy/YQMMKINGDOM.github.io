6d6adde14943828a0c70a2338ce3f90c933af03e
YQMMKINGDOM <wudingyi1020@163.com>
2022-05-18 16:26:49 +0800

    Site updated: 2022-05-18 16:26:49

--- libs/others/clicklove.js
+++ libs/others/clicklove.js
@@ -2,7 +2,7 @@ var a_idx = 0;
 jQuery(document).ready(function($) {
     $("body").click(function(e) {
         var a = new Array
-        ("富强", "民主", "文明", "和谐", "自由", "平等", "公正", "法治", "爱国", "敬业", "诚信", "友善");
+        ("富强", "民主", "文明", "和谐", "自由", "平等", "公正", "法治", "爱国", "敬业", "诚信", "友善","加油","努力","摸鱼中");
         var $i = $("<span/>").text(a[a_idx]);
         a_idx = (a_idx + 1) % a.length;
         var x = e.pageX,
